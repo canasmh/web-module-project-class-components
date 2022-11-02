d6dd6fa0039d71a9a848b1e16d4fc4ac9893c3ff
canasmh <canasmh@yahoo.com>
2022-11-02 00:41:03 -0400

    Reinster the form element. Add another button to hide/show complete

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -4,8 +4,11 @@ export default class Form extends React.Component {
   render() {
     return (
       <div>
-        <input type="text" placeholder='New Todo Item' />
-        <button>Add Item</button>
+      <form onSubmit={this.props.addTodoItem}></form>
+        <input type="text" placeholder='New Todo Item' onChange={evt => console.log(evt.target.value)} />
+        <button type="Submit">Add Item</button>
+        <br/>
+        <button type="button">Hide Complete</button>
       </div>
     )
   }
