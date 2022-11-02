0b319d82c640f1fb085c1da02f2e8179306f97d5
canasmh <canasmh@yahoo.com>
2022-11-02 01:51:41 -0400

    Write functions for handle change on input, adding items to todo array and todo objects, and the function to toggle show/hide complete

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -6,15 +6,52 @@ export default class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      todoItems: ["Hello", "World", "How", "Are", "You"]
+      todoItems: [],
+      todoObj: [],
+      currentTodo: "",
+      showCompleted: true
     };
   }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const newObj = {
+      name: this.state.currentTodo,
+      id: this.state.todoItems.length,
+      completed: false
+    }
+    this.setState({todoObj: [...this.state.todoObj, newObj]})
+    this.setState({todoItems: [...this.state.todoItems, this.state.currentTodo]})
+    this.setState({currentTodo: ""})
+    console.log(this.state.todoObj);
+  }
+
+  handleChange = (event) => {
+    const change = event.target.value;
+    this.setState({currentTodo: change});
+  }
+
+  setCompleted = (index) => {
+    
+    const newObj = this.state.todoObj[index]
+    const completed = !this.state.todoObj[index].completed
+    newObj.completed = completed;
+    this.setState({todoObj: [...this.state.todoObj.slice(0, index), newObj, ...this.state.todoObj.slice(index+1)]})
+  }
+
+  toggleCompleted = () => {
+    this.setState({showCompleted: !this.state.showCompleted})
+  }
+
+  
+
   render() {
     return (
       <div>
-      <h2>Todo List</h2>
-        <TodoList data={this.state.todoItems} />
-        <Form />
+      
+        <h2>Todo List</h2>
+        <TodoList data={this.state.showCompleted ? this.state.todoObj : this.state.todoObj.filter(item => !item.completed)} setCompleted={this.setCompleted} showCompleted={this.state.showCompleted} />
+        <Form handleSubmit={this.handleSubmit} handleChange={this.handleChange} currentTodo={this.state.currentTodo} toggleCompleted={this.toggleCompleted} showCompleted={this.state.showCompleted} />
       </div>
     )
   }
