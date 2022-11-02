0843b03c9d0db514b8cdd43cecda584eac72cb79
canasmh <canasmh@yahoo.com>
2022-11-02 00:27:23 -0400

    Create todo and todolist components

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import TodoList from './TodoList'
 import Form from './Form'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todoItems: ["Hello", "World", "How", "Are", "You"]
+    };
+  }
   render() {
     return (
       <div>
+      <h2>Todo List</h2>
+        <TodoList data={this.state.todoItems} />
         <Form />
       </div>
     )
--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Todo from './Todo.js';
 
 export default class TodoList extends React.Component {
   render() {
     return (
       <div>
-        TodoList
+        <ul>
+          {this.props.data.map(item => <Todo todoItem={item}/>)}
+        </ul>
       </div>
     )
   }
